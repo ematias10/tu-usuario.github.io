2fe92c76babf2ff10a947db2698ddd6709dcc5ca
ematias10 <ematiasal9@gmail.com>
2025-10-04 01:20:31 -0300

    Refactor project configuration: update jsconfig.json, add deploy script in package.json, and adjust base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/mi-portafolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
